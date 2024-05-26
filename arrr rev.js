e86089a4a564d15c68d68db2df0ad1ff89dc7b1a
salikur999 <139066109+salikur999@users.noreply.github.com>
2024-05-26 14:22:29 +0600

    Delete arrr rev.js

--- arrr rev.js
+++
@@ -1,9 +0,0 @@
-var arr = [4,5,6,7,8,10]
-for(var i=0; i<(arr.length /2); i++)
-{
-    var temp=arr[i]
-    arr[i]=arr[arr.length-1-i]
-    arr[arr.length-1-i] = temp
-}
-console.log(arr)
-console.log(arr.reverse())
